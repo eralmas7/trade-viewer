47649ed500b1b2c975176b1bf3de3f762ce9d7df
eralmas7 <eralmas7@yahoo.com>
2022-10-15 21:22:20 +0530

    Removed console logging.

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -25,13 +25,3 @@ var indicesModule = angular
         redirectTo: '/'
       });
   });
-  indicesModule.run([
-  '$rootScope',
-  function($rootScope) {
-    // see what's going on when the route tries to change
-    $rootScope.$on('$locationChangeStart', function(event, newUrl, oldUrl) {
-      // both newUrl and oldUrl are strings
-      console.log('Starting to leave %s to go to %s', oldUrl, newUrl);
-    });
-  }
-]);
